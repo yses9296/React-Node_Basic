734a4153654ac070b59a5e5562c3c1c6060ddcb7
Eunseo Choi <yses9296@gmail.com>
2022-11-08 15:52:27 +0900

    Basic 07

--- node-react-basic/index.js
+++ node-react-basic/index.js
@@ -6,13 +6,31 @@ const port = 4000
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://yses9296:jj0709ang25@cluster0.vei6kr9.mongodb.net/?retryWrites=true&w=majority')
 .then( ()=> console.log('MongoDB Connected..')) //MongoDB Connection Succeed
-.catch( err => console.log(err)) //MongoDB Connection Failed
+.catch( err => console.log(err))                //MongoDB Connection Failed
 
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//Register Route
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true})); //application/x-www-form-urlencoded 분석
+app.use(bodyParser.json()); //application/json 분석
+
+const { User } = require('./models/User');
+
+app.post('/register', (req, res) => {//회원가입에 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body); //인스턴스 생성
+  user.save((err, userInfo) => {
+    if(err) return res.json( {success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
