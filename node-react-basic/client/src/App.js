b635badf759255c0fd54205d668ff77cd9444c88
Eunseo Choi <yses9296@gmail.com>
2022-11-09 14:22:08 +0900

    Basic 20

--- node-react-basic/client/src/App.js
+++ node-react-basic/client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import NavBar from './components/views/NavBar/NavBar';
+import Footer from './components/views/Footer/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>React Router</h1>
+      
+        <Routes>
+          <Route path="/" element={<LandingPage/>}></Route>
+          <Route path="/login*" element={<LoginPage/>}> </Route>
+          <Route path="/register" element={<RegisterPage/>}> </Route>
+          <Route path="/navbar" element={<NavBar/>}> </Route>
+          <Route path="/footer" element={<Footer/>}> </Route>
+        </Routes>
+      </div>
+      </Router>
   );
+
+
 }
 
 export default App;
---
+++ node-react-basic/client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default LandingPage;
